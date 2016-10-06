369b31010de8652724cbc3a2e971fcf78c4ddfd6
Justin Schellenberg <jrschellenberg@Gmail.com>
2016-10-06 16:50:42 -0500

    Initial commit

---
+++ js/quotes.js
@@ -0,0 +1,67 @@
+/**
+ * Created by Justin on 10/4/2016.
+ * Quotes Taken from https://gist.github.com/signed0/d70780518341e1396e11
+ * atm just hardcopied
+ */
+
+
+var quotes = [
+    {
+        quote: "Life isn’t about getting and having, it’s about giving and being.",
+        source: "Kevin Kruse",
+        citation: "test",
+        year: 110
+    },
+    {
+        quote: "Whatever the mind of man can conceive and believe, it can achieve.",
+        source: "Napoleon Hill",
+        citation: "test"
+    },
+    {
+        quote: "Strive not to be a success, but rather to be of value.",
+        source: "Albert Einstein",
+        year: 1990
+    },
+    {
+        quote: "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
+        source: "Robert Frost"
+    },
+    {
+        quote: "I attribute my success to this: I never gave or took any excuse.",
+        source: "Florence Nightingale"
+    },
+    {
+        quote: "You miss 100% of the shots you don’t take.",
+        source: "Wayne Gretzky",
+
+    },
+    {
+        quote: "I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times" +
+        " I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again" +
+        " in my life. And that is why I succeed.",
+        source: "Michael Jordan"
+
+    },
+    {
+        quote: "The most difficult thing is the decision to act, the rest is merely tenacity.",
+        source: "Amelia Earhart"
+
+    },
+    {
+        quote: "Every strike brings me closer to the next home run.",
+        source: "Babe Ruth"
+
+    },
+    {
+        quote: "Life is what happens to you while you’re busy making other plans.",
+        source: "John Lennon"
+
+    },
+    {
+        quote: "The most common way people give up their power is by thinking they don’t have any.",
+        source: "Alice Walker"
+
+    }
+
+
+];
---
+++ js/script.js
@@ -0,0 +1,74 @@
+//Setting up a second array as a hardcopy not a reference.
+var secondQuoteArray = [];
+for(var i=0; i<quotes.length; i++){
+    secondQuoteArray[i] = quotes[i];
+}
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+//Taking from the Mozilla Developer Website
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// A function that takes the result from the GetRandomQuote() function
+function printQuote() {
+    var theQuote = getRandomQuote();
+    var message;
+    if(theQuote.citation===undefined && theQuote.year===undefined){
+        message = "<p class=\"quote\">"+ theQuote.quote + "</p> <p class=\"source\">" + theQuote.source + " </p>";
+    }else if(theQuote.citation === undefined){
+        message = "<p class=\"quote\">"+ theQuote.quote + "</p> <p class=\"source\">" + theQuote.source +
+            "<span class=\"year\">"+ theQuote.year + "</span> </p>";
+    }else if(theQuote.year === undefined) {
+        message = "<p class=\"quote\">"+ theQuote.quote + "</p> <p class=\"source\">" + theQuote.source +
+            " <span class=\"citation\">"+ theQuote.citation + "</span> </p>";
+    }else{
+        message = "<p class=\"quote\">"+ theQuote.quote +"</p> <p class=\"source\">" + theQuote.source+" <span class=\"citation\">"+
+        theQuote.citation + "</span> <span class=\"year\">"+ theQuote.year + "</span> </p>";
+    }
+    console.log(message);
+    //Code taken from https://www.paulirish.com/2009/random-hex-color-code-snippets/
+    //Code used to select a random color.
+    var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+    document.getElementsByTagName("BODY")[0].style.backgroundColor = color;
+    document.getElementById("loadQuote").style.backgroundColor = color;
+    document.getElementById('quote-box').innerHTML = message;
+
+}
+/* A function that returns a random quote as a tempQuote.
+  Removes the quotes randomly selected from the quotes array until non are left. Then repopulates the array with
+  quotes.
+  Function stores the randomly selected index in the var index variable. then works on the array with that index value.
+ */
+function getRandomQuote(){
+    //once no more values left in quote, repopulate array with the values from the array copy.
+    if(quotes.length===0){
+        console.log("in the if statement.");
+        for(var i=0; i<secondQuoteArray.length; i++){
+            quotes[i] = secondQuoteArray[i];
+        }
+    }
+    //Debugging console logs.
+    //console.log("the quotes 2 array.....");
+    //console.log(secondQuoteArray);
+    var index = getRandomIntInclusive(0,quotes.length-1);
+    var tempQuote = quotes[index];
+    //removing the value from the array at the index, index
+    quotes.splice(index, 1);
+    //console.log("length of array");
+    //console.log(quotes.length);
+    return tempQuote;
+}
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+//This code executes the printQuote function every 10 seconds.
+setInterval(printQuote, 10* 1000);
+
+
+
+
+
+
